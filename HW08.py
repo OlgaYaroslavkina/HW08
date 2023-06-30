17cc9c9a3b3491fadfe0b3932fa1723562c92186
OlgaYaroslavkina <131812667+OlgaYaroslavkina@users.noreply.github.com>
2023-06-30 11:44:09 +0300

    Зміни30.06.23

--- HW08.py
+++ HW08.py
@@ -3,25 +3,25 @@ from collections import defaultdict
 
 
 employees = [
-    {"name": "Pitier", "birthdate": datetime(1990, 6, 28)},
-    {"name": "Angel", "birthdate": "24.06.2000"},
-    {"name": "Angel", "birthdate": datetime(1985, 6, 28)},
-    {"name": "Bill", "birthdate": datetime(2023, 6, 29)},
-    {"name": "Jill", "birthdate": datetime(2023, 6, 27)},
-    {"name": "Kim", "birthdate": datetime(2023, 6, 28)},
-    {"name": "Jan", "birthdate": datetime(2023, 6, 28)},
+    {"name": "Pitier", "birthdate": datetime(1990, 7, 2)},
+    {"name": "Leon", "birthdate": "04.07.2000"},
+    {"name": "Angel", "birthdate": datetime(1985, 7, 3)},
+    {"name": "Bill", "birthdate": datetime(2023, 7, 4)},
+    {"name": "Jill", "birthdate": datetime(2023, 7, 1)},
+    {"name": "Kim", "birthdate": datetime(2023, 7, 6)},
+    {"name": "Jan", "birthdate": datetime(2023, 7, 5)},
 ]
 
 
 def get_period() -> tuple[datetime.date, datetime.date]:
-    current_date = datetime(year=2023, month=6, day=24)
+    current_date = datetime.now()
     start_period = current_date + timedelta(days=5 - current_date.weekday())
     return start_period.date(), (start_period + timedelta(6)).date()
 
 
 def check_epl(list_of_emp: list) -> None:
     result = defaultdict(list)
-    current_year = datetime(year=2023, month=6, day=24).year
+    current_year = datetime.now().year
     for employee in list_of_emp:
         bd = employee["birthdate"]
         if isinstance(bd, datetime):
@@ -34,7 +34,9 @@ def check_epl(list_of_emp: list) -> None:
 
         if start <= bd <= end:
             if bd.weekday() in (5, 6):
-                result[bd].append(employee["name"])
+                result[start + timedelta(days=2)].append(
+                    employee["name"]
+                )  # Перенести на понеділок
             else:
                 result[bd].append(employee["name"])
     return result
